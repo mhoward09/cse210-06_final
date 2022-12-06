4c11d8a92d1114f5ca8e89807ed3ea3aa2673aea
sumapri <114256701+sumapri@users.noreply.github.com>
2022-12-06 10:33:42 -0600

    Update collide_racket_action.py
    changed racket to ship

--- space_invaders/game/scripting/collision_actions/collide_racket_action.py
+++ space_invaders/game/scripting/collision_actions/collide_racket_action.py
@@ -3,7 +3,7 @@ from game.casting.sound import Sound
 from game.scripting.action import Action
 
 
-class CollideRacketAction(Action):
+class CollideShipAction(Action):
 
     def __init__(self, physics_service, audio_service):
         self._physics_service = physics_service
@@ -11,12 +11,12 @@ class CollideRacketAction(Action):
         
     def execute(self, cast, script, callback):
         ball = cast.get_first_actor(BALL_GROUP)
-        racket = cast.get_first_actor(RACKET_GROUP)
+        ship = cast.get_first_actor(SHIP_GROUP)
         
         ball_body = ball.get_body()
-        racket_body = racket.get_body()
+        ship_body = ship.get_body()
 
-        if self._physics_service.has_collided(ball_body, racket_body):
+        if self._physics_service.has_collided(ball_body, ship_body):
             ball.bounce_y()
             sound = Sound(BOUNCE_SOUND)
             self._audio_service.play_sound(sound)    
