96c80a4e3b099b099d461fd40545e42da4503ef4
sumapri <114256701+sumapri@users.noreply.github.com>
2022-12-06 10:31:32 -0600

    Update collide_brick_action.py
    Changed the bricks words to alien

--- space_invaders/game/scripting/collision_actions/collide_brick_action.py
+++ space_invaders/game/scripting/collision_actions/collide_brick_action.py
@@ -3,7 +3,7 @@ from game.casting.sound import Sound
 from game.scripting.action import Action
 
 
-class CollideBrickAction(Action):
+class CollideAlienAction(Action):
 
     def __init__(self, physics_service, audio_service):
         self._physics_service = physics_service
@@ -11,17 +11,17 @@ class CollideBrickAction(Action):
         
     def execute(self, cast, script, callback):
         ball = cast.get_first_actor(BALL_GROUP)
-        bricks = cast.get_actors(BRICK_GROUP)
+        alien = cast.get_actors(ALIEN_GROUP)
         stats = cast.get_first_actor(STATS_GROUP)
         
-        for brick in bricks:
+        for alien in bricks:
             ball_body = ball.get_body()
-            brick_body = brick.get_body()
+            alien_body = alien.get_body()
 
-            if self._physics_service.has_collided(ball_body, brick_body):
+            if self._physics_service.has_collided(ball_body, alien_body):
                 ball.bounce_y()
                 sound = Sound(BOUNCE_SOUND)
                 self._audio_service.play_sound(sound)
-                points = brick.get_points()
+                points = alien.get_points()
                 stats.add_points(points)
-                cast.remove_actor(BRICK_GROUP, brick)
+                cast.remove_actor(ALIEN_GROUP, alien)
