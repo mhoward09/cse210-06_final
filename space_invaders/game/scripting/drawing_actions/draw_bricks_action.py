08a116d80d4d8cb026547910eb265cf989347b01
sumapri <114256701+sumapri@users.noreply.github.com>
2022-12-06 10:36:36 -0600

    Update draw_bricks_action.py
    same

--- space_invaders/game/scripting/drawing_actions/draw_bricks_action.py
+++ space_invaders/game/scripting/drawing_actions/draw_bricks_action.py
@@ -2,22 +2,22 @@ from constants import *
 from game.scripting.action import Action
 
 
-class DrawBricksAction(Action):
+class DrawAliensAction(Action):
 
     def __init__(self, video_service):
         self._video_service = video_service
         
     def execute(self, cast, script, callback):
-        bricks = cast.get_actors(BRICK_GROUP)
+        alien = cast.get_actors(ALIEN_GROUP)
         
-        for brick in bricks:
-            body = brick.get_body()
+        for alien in aliens:
+            body = alien.get_body()
 
-            if brick.is_debug():
+            if alien.is_debug():
                 rectangle = body.get_rectangle()
                 self._video_service.draw_rectangle(rectangle, PURPLE)
                 
-            animation = brick.get_animation()
+            animation = alien.get_animation()
             image = animation.next_image()
             position = body.get_position()
             self._video_service.draw_image(image, position)
